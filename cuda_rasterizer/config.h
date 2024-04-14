fb35633c4bebb2bd75aaa53656d3e8d04aaad4f4
yunjinli <yunjin.li0817@gmail.com>
2024-04-14 16:03:30 +0200

    change 3d feature dimension to 32

--- cuda_rasterizer/config.h
+++ cuda_rasterizer/config.h
@@ -3,7 +3,7 @@
  * Gaussian-Grouping research group, https://github.com/lkeab/gaussian-grouping
  * All rights reserved.
  * ------------------------------------------------------------------------
- * Modified from codes in Gaussian-Splatting 
+ * Modified from codes in Gaussian-Splatting
  * GRAPHDECO research group, https://team.inria.fr/graphdeco
  */
 
@@ -11,7 +11,8 @@
 #define CUDA_RASTERIZER_CONFIG_H_INCLUDED
 
 #define NUM_CHANNELS 3 // Default 3, RGB
-#define NUM_OBJECTS 16 // Default 16, identity encoding
+// #define NUM_OBJECTS 16 // Default 16, identity encoding
+#define NUM_OBJECTS 32 // Default 16, identity encoding
 #define BLOCK_X 16
 #define BLOCK_Y 16
 
